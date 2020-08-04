319068c819f0a3a148506e2f3b0fb3d7be359a2a
Sergey Malyshev <in1t@ya.ru>
2020-08-05 00:32:24 +0300

    a

--- srcs/asm/lexer/lexer_comment.c
+++ srcs/asm/lexer/lexer_comment.c
@@ -1,21 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lexer_get_next_token.c                             :+:      :+:    :+:   */
+/*   lexer_comment.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bconchit <bconchit@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/08/05 00:03:39 by bconchit          #+#    #+#             */
-/*   Updated: 2020/08/05 00:07:30 by bconchit         ###   ########.fr       */
+/*   Created: 2020/08/05 00:11:14 by bconchit          #+#    #+#             */
+/*   Updated: 2020/08/05 00:32:15 by bconchit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-t_token		*lexer_get_next_token(t_lexer *self)
+void	lexer_comment(t_lexer *self)
 {
-	char	ch;
+	// t_token		*token;
 
-	ch = lexer_peek(self);
+	// while ()
+	// {
+	// 	ch == 
+	// 	scanner_start(self->scanner);
+	// }
 	
+	// lexer_startinit_token(self);
+	// lexer_goto_not_in(self, );
+	// lexer_make_token(self);
+
+
+// 	scanner_next_not_in(self->scanner, "\n\0");
+
+// 	goto '\n\0'
+
+// 	token = token_create(COMMENT, );
+// 	token->type = COMMENT;
+	
+
+// 	# goto '\n\0'
 }
