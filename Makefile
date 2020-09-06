0e5d5d1a086ad5c27f61cb0c5c2fb3ffca707f59
in1t <in1t@ya.ru>
2020-09-07 00:14:08 +0300

    a

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: bconchit <bconchit@student.21-school.ru>   +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/06/25 20:00:06 by bconchit          #+#    #+#              #
-#    Updated: 2020/09/06 19:51:15 by bconchit         ###   ########.fr        #
+#    Updated: 2020/09/07 00:13:04 by bconchit         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -159,7 +159,7 @@ norm:
 	@norminette $(INCS) $(SRCS)
 
 vv: $(NAME)
-	valgrind --leak-check=full --show-leak-kinds=all ./$(NAME) ./_res/champs/42.s ./_res/champs/ex.s
-	#valgrind ./$(NAME) ./_res/champs/42.s ./_res/champs/ex.s
+	#valgrind --leak-check=full --show-leak-kinds=all ./$(NAME) ./_res/champs/42.s ./_res/champs/ex.s
+	valgrind --leak-check=full --show-leak-kinds=all ./$(NAME) `find . -type f -name "*.s"`
 
 .PHONY: all clean fclean re norm vv
--- srcs/asm/classes/argument/argument_print.c
+++ srcs/asm/classes/argument/argument_print.c
@@ -6,7 +6,7 @@
 /*   By: bconchit <bconchit@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/06 00:32:32 by bconchit          #+#    #+#             */
-/*   Updated: 2020/09/06 00:59:23 by bconchit         ###   ########.fr       */
+/*   Updated: 2020/09/06 23:56:50 by bconchit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,13 @@ void	argument_print(t_argument *self)
 {
 	ft_printf("[%03d:%03d] ARGUMENT: type =",
 		self->token->ln, self->token->col);
-	(self->arg_type & T_REG) && ft_printf(" T_REG");
-	(self->arg_type & T_DIR) && ft_printf(" T_DIR");
-	(self->arg_type & T_IND) && ft_printf(" T_IND");
-	(self->arg_type & T_LAB) && ft_printf(" T_LAB");
+	if (self->arg_type & T_REG)
+		ft_printf(" T_REG");
+	if (self->arg_type & T_DIR)
+		ft_printf(" T_DIR");
+	if (self->arg_type & T_IND)
+		ft_printf(" T_IND");
+	if (self->arg_type & T_LAB)
+		ft_printf(" T_LAB");
 	ft_printf(", size = %lu, value = %d", self->size, self->number);
 }
