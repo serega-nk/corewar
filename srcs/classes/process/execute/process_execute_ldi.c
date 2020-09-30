7411941a3550588d44546279798a0a79473ec29b
Sergey Malyshev <in1t@ya.ru>
2020-10-01 02:39:26 +0300

    a

--- srcs/classes/process/execute/process_execute_ldi.c
+++ srcs/classes/process/execute/process_execute_ldi.c
@@ -6,7 +6,7 @@
 /*   By: bconchit <bconchit@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 11:22:10 by bconchit          #+#    #+#             */
-/*   Updated: 2020/09/30 12:38:44 by bconchit         ###   ########.fr       */
+/*   Updated: 2020/10/01 02:38:27 by bconchit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,5 @@ void		process_execute_ldi(t_process *self, int v[3], t_arg_type t[3])
 	rel0 = process_get(self, t[0], v[0]);
 	rel1 = process_get(self, t[1], v[1]);
 	self->reg[v[2]] = process_read(self, rel0 + rel1);
+	self->carry = (self->reg[v[2]] == 0);
 }
--- srcs/classes/process/execute/process_execute_lldi.c
+++ srcs/classes/process/execute/process_execute_lldi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   process_execute_lldi.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jremarqu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: bconchit <bconchit@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 09:39:40 by bconchit          #+#    #+#             */
-/*   Updated: 2020/09/30 23:36:03 by jremarqu         ###   ########.fr       */
+/*   Updated: 2020/10/01 02:38:19 by bconchit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,5 @@ void		process_execute_lldi(t_process *self, int v[3], t_arg_type t[3])
 	rel0 = process_lget(self, t[0], v[0]);
 	rel1 = process_lget(self, t[1], v[1]);
 	self->reg[v[2]] = process_lread(self, rel0 + rel1);
+	self->carry = (self->reg[v[2]] == 0);
 }
