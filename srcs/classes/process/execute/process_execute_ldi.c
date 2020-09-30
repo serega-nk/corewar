c1b29b2de6ec1be09b4051e5f28b5787fb61950a
Sergey Malyshev <in1t@ya.ru>
2020-09-30 11:42:40 -0700

    a

--- srcs/classes/process/execute/process_execute_ldi.c
+++ srcs/classes/process/execute/process_execute_ldi.c
@@ -6,7 +6,7 @@
 /*   By: bconchit <bconchit@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 11:22:10 by bconchit          #+#    #+#             */
-/*   Updated: 2020/09/30 11:22:13 by bconchit         ###   ########.fr       */
+/*   Updated: 2020/09/30 11:42:29 by bconchit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,20 @@
 
 void	process_execute_ldi(t_process *self)
 {
-	if (self)
-		return ;
+	int		a;
+	int		b;
+	long	rel;
+
+	if (self->arg_types[0] & T_DIR)
+		a = self->args[0];
+	else if (self->arg_types[0] & T_IND)
+		a = vm_read_int(self->vm, self->pc + (self->args[0] % IDX_MOD));
+	else
+		a = self->reg[self->args[0]];
+	if (self->arg_types[1] & T_DIR)
+		b = self->args[1];
+	else
+		b = self->reg[self->args[1]];	
+	self->reg[self->args[2]] = vm_read_int(self->vm,
+		self->pc + ((a + b) % IDX_MOD));
 }
