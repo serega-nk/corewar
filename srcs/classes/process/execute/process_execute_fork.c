30bf4a0e2d7bd492d45cfa9082fb033f12eb8111
Sergey Malyshev <in1t@ya.ru>
2020-10-01 01:49:43 +0300

    a

--- srcs/classes/process/execute/process_execute_fork.c
+++ srcs/classes/process/execute/process_execute_fork.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   process_execute_fork.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jremarqu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: bconchit <bconchit@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 09:39:49 by bconchit          #+#    #+#             */
-/*   Updated: 2020/09/30 23:32:16 by jremarqu         ###   ########.fr       */
+/*   Updated: 2020/10/01 01:47:38 by bconchit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ void	process_execute_fork(t_process *self, int v[3])
 {
 	t_process	*clone;
 
-	clone = process_clone(self, v[0] % IDX_MOD);
+	clone = process_clone(self, self->pc + v[0] % IDX_MOD);
 	list_push_back(self->vm->fork_processes, clone);
 }
