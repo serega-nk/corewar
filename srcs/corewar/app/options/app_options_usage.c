af8dde14e1197b9fef0d043e71d5e78b1830c2b2
Orkhan (Jremarque) <jremarqu@student.21-school.ru>
2020-10-01 02:04:56 +0300

    jremarqu changed the corewar -> usage

--- srcs/corewar/app/options/app_options_usage.c
+++ srcs/corewar/app/options/app_options_usage.c
@@ -6,7 +6,7 @@
 /*   By: jremarqu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/15 13:06:18 by bconchit          #+#    #+#             */
-/*   Updated: 2020/10/01 02:00:40 by jremarqu         ###   ########.fr       */
+/*   Updated: 2020/10/01 02:03:40 by jremarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ void	app_options_usage(t_app *self)
 	{
 		ft_printf("=================================================="
 		"==============================================\n");
-		ft_printf("Usage: ./corewar [-a -s <num> -show <num> -dump <num>"
-		"-d <num> -n <num>] champion.cor> <...>\n");
+		ft_printf("Usage: ./corewar [-a -dump <num>"
+		" -d <num> -n <num>] champion.cor> <...>\n");
 		ft_printf("=================================================="
 		"==============================================\n");
 		ft_printf("\t-a 	      :\tPrint output from \"aff\""
