202fda49b281c0b48913195e05369c095c9747b4
Sergey Malyshev <in1t@ya.ru>
2020-10-01 03:18:32 +0300

    a

--- srcs/classes/process/execute/process_execute_live.c
+++ srcs/classes/process/execute/process_execute_live.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   process_execute_live.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jremarqu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: bconchit <bconchit@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 09:39:24 by bconchit          #+#    #+#             */
-/*   Updated: 2020/10/01 00:13:22 by jremarqu         ###   ########.fr       */
+/*   Updated: 2020/10/01 03:18:06 by bconchit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,10 @@ void	process_execute_live(t_process *self, int v[3])
 
 	self->last_live = self->vm->cycles_num;
 	self->vm->lives_num++;
-	if (vector_get(self->vm->players, -v[0], (void **)&player))
+	if (vector_get(self->vm->players, -v[0] - 1, (void **)&player))
 	{
 		self->vm->winner = player;
-		// player->lives_num++;
-		// ft_printf("A process shows that player %d (%s) is alive\n",
-		// 	player->id, player->name);
+		ft_printf("A process shows that player %d (%s) is alive, cycle = %d\n",
+			player->id, player->name, self->vm->cycles_num);
 	}
 }
