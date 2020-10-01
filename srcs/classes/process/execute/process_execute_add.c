58f38e1396729facb542e881a3a3c006d1e17639
Wisp Zei <wzei@mi-n1.msk.21-school.ru>
2020-10-01 17:52:49 +0300

    reference op_add

--- srcs/classes/process/execute/process_execute_add.c
+++ srcs/classes/process/execute/process_execute_add.c
@@ -3,14 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   process_execute_add.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jremarqu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: wzei <wzei@student.21-school.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 08:46:42 by bconchit          #+#    #+#             */
-/*   Updated: 2020/10/01 03:56:01 by jremarqu         ###   ########.fr       */
+/*   Updated: 2020/10/01 17:52:08 by wzei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "classes.h"
+
+/*
+** INSTRUCTION
+**
+**   add
+**
+** OPCODE
+**
+**   0x04
+**
+** 42 SUBJECT REFERENCE
+**
+**   add: Opcode 4. Take three registries, add the first two, and place the
+**   result in the third, right before modifying the carry.
+**
+** EPITECH SUBJECT REFERENCE
+**
+**   This instruction takes 3 registers as parameter, adds the contents of
+**   the 2 first and stores the result in the third. This operation modifies
+**   the carry. add r2,r3,r5 adds r2 and r3 and stores the result in r5
+**
+** USAGE EXAMPLE
+**
+**   add r2, r3, r2
+**   add r2, r3, r2
+**
+**   {"add", 3, {T_REG,
+**               T_REG,
+**               T_REG},
+**    4, 10, "addition", 1, 0}
+*/
 
 void	process_execute_add(t_process *self, int v[3])
 {
