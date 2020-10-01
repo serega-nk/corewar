7feb923ca40731f9502e3160dfeba2095f8408ea
Sergey Malyshev <in1t@ya.ru>
2020-10-01 03:38:28 +0300

    a

--- srcs/classes/process/process_lread.c
+++ srcs/classes/process/process_lread.c
@@ -6,7 +6,7 @@
 /*   By: bconchit <bconchit@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 12:30:22 by bconchit          #+#    #+#             */
-/*   Updated: 2020/10/01 02:13:02 by bconchit         ###   ########.fr       */
+/*   Updated: 2020/10/01 03:37:53 by bconchit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int		process_lread(t_process *self, long rel)
 {
-	short	value;
+	int		value;
 
 	vm_read(self->vm, self->pc + rel, &value, sizeof(value));
 	ft_memrev(&value, sizeof(value));	
