12150df40d63dcefc0b4679047a6e0f2c3b5b4cf
Orkhan (Jremarque) <jremarqu@student.21-school.ru>
2020-10-01 18:20:36 +0300

    process_execute_sub verbosity

--- srcs/classes/process/execute/process_execute_sub.c
+++ srcs/classes/process/execute/process_execute_sub.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   process_execute_sub.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wzei <wzei@student.21-school.ru>           +#+  +:+       +#+        */
+/*   By: jremarqu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 09:00:10 by bconchit          #+#    #+#             */
-/*   Updated: 2020/10/01 17:58:25 by wzei             ###   ########.fr       */
+/*   Updated: 2020/10/01 18:20:14 by jremarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,4 +43,7 @@ void	process_execute_sub(t_process *self, int v[3])
 {
 	self->reg[v[2]] = self->reg[v[0]] - self->reg[v[1]];
 	self->carry = (self->reg[v[2]] == 0);
+	if (self->vm->verbosity & VERBOSITY_OPERATIONS)
+		ft_printf("Process %4d | sub r%d r%d r%d\n", self->id, v[0], v[1],
+		v[2]);
 }
