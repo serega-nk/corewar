10fb0d92e5a11092ea75164117aa8d363ff8d75e
Orkhan (Jremarque) <jremarqu@student.21-school.ru>
2020-10-01 18:38:28 +0300

    VERBOSITY_DEATHS proces -> process_desdroy NEED TO CHECK

--- srcs/classes/process/process_destroy.c
+++ srcs/classes/process/process_destroy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   process_destroy.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bconchit <bconchit@student.21-school.ru>   +#+  +:+       +#+        */
+/*   By: jremarqu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/15 19:17:06 by bconchit          #+#    #+#             */
-/*   Updated: 2020/09/28 22:15:03 by bconchit         ###   ########.fr       */
+/*   Updated: 2020/10/01 18:37:08 by jremarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@ void	process_destroy(t_process **aself)
 {
 	if (aself && *aself)
 	{
+		if ((*aself)->vm->verbosity & VERBOSITY_DEATHS)
+			ft_printf("Process %u hasn't lived for %d cycles (Cycles_to_die %d)\n", (*aself)->id,
+			(*aself)->vm->cycles_num - (*aself)->last_live, (*aself)->vm->cycles_to_die);
 		vector_clean((*aself)->arguments, &argument_destroy);
 		vector_destroy(&(*aself)->arguments);
 		ft_memdel((void **)aself);
